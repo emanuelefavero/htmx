a6a260ec43396a6a29899da0c614e9ef712dd6e1
emanuelefavero <emafaux89@gmail.com>
2024-04-13 19:20:23 +0200

    refactor index.js

--- index.js
+++ index.js
@@ -2,16 +2,19 @@ const express = require('express')
 const app = express()
 const PORT = 3000
 
-app.get('/posts', (req, res) => {
-  res.sendFile(__dirname + '/views/posts.html')
-})
+// Serve static files from public directory
+app.use(express.static('public'))
+
+// Middleware to parse request bodies
+app.use(express.urlencoded({ extended: true }))
 
 app.get('/home', (req, res) => {
   res.sendFile(__dirname + '/public/index.html')
 })
 
-// Serve static files from public directory
-app.use(express.static('public'))
+app.get('/posts', (req, res) => {
+  res.sendFile(__dirname + '/views/posts.html')
+})
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`)
