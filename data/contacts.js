0a81653da2ccb3124078e4637e457dd15dc20079
emanuelefavero <emafaux89@gmail.com>
2024-04-13 20:04:51 +0200

    add search contacts

--- data/contacts.js
+++ data/contacts.js
@@ -1,8 +1,6 @@
-const contacts = [
+export const contacts = [
   { firstName: 'John', lastName: 'Doe', email: 'john.doe@example.com' },
   { firstName: 'Jane', lastName: 'Doe', email: 'jane.doe@example.com' },
   { firstName: 'Alice', lastName: 'Smith', email: 'alice.smith@example.com' },
   { firstName: 'Bob', lastName: 'Brown', email: 'bob.brown@example.com' },
 ]
-
-module.exports = contacts
--- routes/api.js
+++ routes/api.js
@@ -2,15 +2,56 @@
 import { Router } from 'express'
 import path from 'path'
 import { fileURLToPath } from 'url'
+import { contacts } from '../data/contacts.js'
 
 const apiRouter = Router()
 
 // Calculate directory path
 const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
-// API route for posts
+// API routes
+// * posts
 apiRouter.get('/posts', (req, res) => {
   res.sendFile(path.join(__dirname, '../api/posts.html'))
+})
+
+// * contacts
+apiRouter.get('/all-contacts', (req, res) => {
+  const resultsHTML = contacts
+    .map(
+      (contact) => `
+    <li>
+      <strong>${contact.firstName} ${contact.lastName}</strong>
+      <span>${contact.email}</span>
+    </li>
+  `
+    )
+    .join('')
+  res.send(resultsHTML)
+})
+
+apiRouter.post('/search-contacts', (req, res) => {
+  const query = req.body.search.toLowerCase()
+  const filteredContacts = contacts.filter((contact) => {
+    return (
+      contact.firstName.toLowerCase().includes(query) ||
+      contact.lastName.toLowerCase().includes(query) ||
+      contact.email.toLowerCase().includes(query)
+    )
+  })
+
+  const resultsHTML = filteredContacts
+    .map((contact) => {
+      return `
+      <li>
+        <strong>${contact.firstName} ${contact.lastName}</strong>
+        <span>${contact.email}</span>
+      </li>
+    `
+    })
+    .join('') // Join the array of strings into a single string
+
+  res.send(resultsHTML)
 })
 
 export default apiRouter
