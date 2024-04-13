b543fbf87e67da79d347b634f39b9a5d3943da92
emanuelefavero <emafaux89@gmail.com>
2024-04-13 19:38:56 +0200

    refactor code to add pages and api directories, add es6

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
-const express = require('express')
+import express from 'express'
+import path from 'path'
+import { fileURLToPath } from 'url'
 const app = express()
 const PORT = 3000
+
+// Simulate __dirname (in es6 __dirname is not available)
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
 // Serve static files from public directory
 app.use(express.static('public'))
@@ -8,12 +14,22 @@ app.use(express.static('public'))
 // Middleware to parse request bodies
 app.use(express.urlencoded({ extended: true }))
 
+// * Pages
 app.get('/home', (req, res) => {
   res.sendFile(__dirname + '/public/index.html')
 })
 
 app.get('/posts', (req, res) => {
-  res.sendFile(__dirname + '/views/posts.html')
+  res.sendFile(__dirname + '/pages/posts.html')
+})
+
+app.get('/contacts', (req, res) => {
+  res.sendFile(__dirname + '/pages/contacts.html')
+})
+
+// * API
+app.get('/api/posts', (req, res) => {
+  res.sendFile(__dirname + '/api/posts.html')
 })
 
 app.listen(PORT, () => {
